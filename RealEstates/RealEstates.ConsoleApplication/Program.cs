d4d4dcb48257b84f131e7883a748e34b472f2af4
Kosta Dimitrov <kostadimitrov19@gmail.com>
2023-11-14 10:38:39 +0200

    Make importance of tag nullable

--- RealEstates/RealEstates.ConsoleApplication/Program.cs
+++ RealEstates/RealEstates.ConsoleApplication/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Models;
+using RealEstates.Services;
+using System;
 
 namespace RealEstates.ConsoleApplication
 {
@@ -6,7 +10,62 @@ namespace RealEstates.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var context = new ApplicationDbContext();
+            //context.Database.Migrate();
+
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose an option");
+                Console.WriteLine("1. Property search");
+                Console.WriteLine("2. Most expensive districts");
+                Console.WriteLine("3. Average price per square meter");
+                bool parsed = int.TryParse(Console.ReadLine(), out int option);
+
+                if (parsed && option >= 1 && option <= 3)
+                {
+                    switch (option)
+                    {
+                        case 1: PropertySearch(context);break;
+                        case 2: MostExpensiveDistricts(context); break;
+                        case 3: AveragePricePerSquareMeter(context); break;
+                        default:
+                            break;
+                    }
+
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        private static void AveragePricePerSquareMeter(ApplicationDbContext context)
+        {
+            IPropertiesService propertiesService = new PropertiesService(context);
+            Console.WriteLine("Average price per square meter: " + propertiesService.AveragePricePerSquareMeter());
+        }
+
+        private static void MostExpensiveDistricts(ApplicationDbContext context)
+        {
+            Console.Write("District count:");
+            int districtsCount = int.Parse(Console.ReadLine());
+            IDistrictsService service = new DistrictService(context);
+            var districts = service.GetMostExpensiveDistricts(districtsCount);
+        }
+
+        private static void PropertySearch(ApplicationDbContext context)
+        {
+            Console.Write("Min price:");
+            int minPrice = int.Parse(Console.ReadLine());
+            Console.Write("Max price:");
+            int maxPrice = int.Parse(Console.ReadLine());
+            Console.Write("Min size:");
+            int minSize = int.Parse(Console.ReadLine());
+            Console.Write("Max size:");
+            int maxSize = int.Parse(Console.ReadLine());
+
+            IPropertiesService service = new PropertiesService(context);
+            var properties = service.Search(minPrice, maxPrice, minSize, maxSize);
         }
     }
 }
--- RealEstates/RealEstates.Data/Migrations/20231114083716_Made tag importance nullable.Designer.cs
+++ RealEstates/RealEstates.Data/Migrations/20231114083716_Made tag importance nullable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealEstates.Data;
 
 namespace RealEstates.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231114083716_Made tag importance nullable")]
+    partial class Madetagimportancenullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
