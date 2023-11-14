c84e8efb0b3b77bb4f96c57630d44e6ef985ab46
Kosta Dimitrov <kostadimitrov19@gmail.com>
2023-11-14 08:12:24 +0200

    Added service models

---
+++ RealEstates/RealEstates.Services/Models/DistrictInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstates.Services.Models
+{
+    public class DistrictInfo
+    {
+        public string Name { get; set; }
+        public decimal AveragePricePerSquareMeter { get; set; }
+        public int PropertiesCount { get; set; }
+    }
+}
