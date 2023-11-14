c59bbb32182d4203a0e0e71be92403ea5ad7e45b
Kosta Dimitrov <kostadimitrov19@gmail.com>
2023-11-14 08:14:04 +0200

    Added service interfaces

---
+++ RealEstates/RealEstates.Services/IDistrictsService.cs
@@ -0,0 +1,10 @@
+﻿using RealEstates.Services.Models;
+using System.Collections.Generic;
+
+namespace RealEstates.Services
+{
+    public interface IDistrictsService
+    {
+        IEnumerable<DistrictInfo> GetMostExpensiveDistricts(int count);
+    }
+}
---
+++ RealEstates/RealEstates.Services/IPropertiesService.cs
@@ -0,0 +1,12 @@
+﻿using RealEstates.Services.Models;
+using System.Collections.Generic;
+
+namespace RealEstates.Services
+{
+    public interface IPropertiesService
+    {
+        void Add(string district, int floor, int maxFloor, int size,
+            int yardSize, int year, string propertyType, string buildingType, int price);
+        IEnumerable<PropertyInfo> Search(int minPrice, int maxPrice, int minSize, int maxSize);
+    }
+}
