0c5d00439c1e0f9e0f3a53934ef4018367cd6a62
Kosta Dimitrov <kostadimitrov19@gmail.com>
2023-11-14 15:33:22 +0200

    Added logic for desializing json and adding the records to database

--- RealEstates/RealEstates.Importer/Program.cs
+++ RealEstates/RealEstates.Importer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RealEstates.Data;
+using RealEstates.Services;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace RealEstates.Importer
 {
@@ -6,7 +10,30 @@ namespace RealEstates.Importer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dbContext = new ApplicationDbContext();
+            IPropertiesService propertiesService = new PropertiesService(dbContext);
+            ImportJsonFile("imot.bg-houses-Sofia-raw-data-2021-03-18.json", propertiesService);
+            ImportJsonFile("imot.bg-raw-data-2021-03-18.json", propertiesService);
+        }
+
+        public static void ImportJsonFile(string path, IPropertiesService propertiesService)
+        {
+            var properties =
+                 JsonSerializer.Deserialize<IEnumerable<PropertyAsJson>>(File.ReadAllText(path));
+
+            foreach (var item in properties)
+            {
+                propertiesService.Add(
+                    item.District,
+                    item.Floor,
+                    item.TotalFloors,
+                    item.Size,
+                    item.YardSize,
+                    item.Year,
+                    item.Type,
+                    item.BuildingType,
+                    item.Price);
+            }
         }
     }
 }
